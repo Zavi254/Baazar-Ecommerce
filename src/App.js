4ce9c792e22180f1c06f5ae7429e7637b19d2d57
Steven-Otieno <steveotianga8991@gmail.com>
2022-07-27 20:21:45 +0300

    Imported the navigations component and the Browser Router and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Cart from "./Components/Cart";
+import Men from "./Components/Men";
+import Women from "./Components/Women";
+import Children from "./Components/Children";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="home" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="men-shoes" element={<Men />} />
+          <Route path="women-shoes" element={<Women />} />
+          <Route path="children-shoes" element={<Children />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
