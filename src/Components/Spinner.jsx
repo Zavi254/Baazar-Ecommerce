e3231d8948afe5b6863a8adf7649fddfcf5b8db6
Steven-Otieno <steveotianga8991@gmail.com>
2022-07-30 18:32:53 +0300

    Added background image

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+
+const Spinner = () => {
+  <div>
+    <h1>Click zz</h1>
+    <div className="loader"></div>;
+  </div>;
+};
+
+export default Spinner;
